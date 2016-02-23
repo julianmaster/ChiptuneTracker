0ab069a002ca1066791668eccea56610a8164263
Julien MAITRE <julienm86230@hotmail.fr>
2016-02-23 18:09:05 +0100

    Test new function for oscillator.

--- src/main/SineSawtoothFunction.java
+++ src/main/SineSawtoothFunction.java
@@ -3,9 +3,11 @@ package main;
 import com.jsyn.data.Function;
 
 public class SineSawtoothFunction implements Function {
+	
+	public static final int H = 20;
 
 	@Override
 	public double evaluate(double input) {
-		return (- 1.16635) * Math.pow(input, 3) + 1.16635 * input;
+		return (input - (Math.tanh(((input+0.5)-Math.floor(input+0.5)-0.5)*H)/(2*Math.tanh(0.5*H))+Math.floor(input+0.5)))*3;
 	}
 }
